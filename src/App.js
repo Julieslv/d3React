aafefb9252bb6f3e99151bf2e495bccdb0ba7ccb
Julz <mail@juliestephanie.com>
2021-03-24 10:59:55 +0000

    add untracked files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import * as d3 from 'd3';
+import Face from './components/face';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import styled from 'styled-components';
+
+console.log(d3);
+
+const FetchData = async dataUrl => {
+	const response = await fetch(dataUrl);
+	// console.log(await response.text());
+	// const data = d3.csvParse(text);
+	// console.log(data);
+	return await response.text();
+};
+
+const dataUrl =
+	'https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/acd2b8cecfe51c520622fbaf407ee88b8796bfc6/cssNamedColors.csv';
+FetchData(dataUrl).then(text => {
+	// console.log(text);
+});
+
+const width = 960;
+const height = 500;
+const circleRadius = 30;
+
+console.log(window.width);
+
+const initialMousePosition = { x: width / 2, y: height / 2 };
+
+const App = () => {
+	const [mousePosition, setMousePosition] = useState(initialMousePosition);
+
+	const handleMouseMove = useCallback(
+		event => {
+			const { clientX, clientY } = event;
+			setMousePosition({ x: clientX, y: clientY });
+		},
+		[setMousePosition]
+	);
+	return (
+		<PageEl>
+			<svg width={width} height={height} onMouseMove={handleMouseMove}>
+				<circle cx={mousePosition.x} cy={mousePosition.y} r={circleRadius} />
+			</svg>
+
+			{/* <Face /> */}
+		</PageEl>
+	);
+};
+
+const PageEl = styled.div`
+	margin: 0 auto;
+	background: pink;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	svg {
+		fill: yellow;
+		background-color: red;
+	}
+`;
 
 export default App;
---
+++ src/components/face/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { arc } from 'd3';
+import styled from 'styled-components';
+
+import Eyes from './eyes';
+import Mouth from './mouth';
+
+console.log(arc);
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 10;
+const eyeOffsetX = 90;
+const eyeOffsetY = 90;
+const eyeRadius = 40;
+const mouthWidth = 15;
+const mouthRadius = 200;
+
+const face = () => {
+	return (
+		<FaceEl>
+			<svg width={width} height={height}>
+				<g transform={`translate(${centerX}, ${centerY})`}>
+					<circle
+						r={centerY - strokeWidth / 2}
+						fill='yellow'
+						stroke='black'
+						strokeWidth={strokeWidth}></circle>
+					<Eyes
+						eyeOffsetX={eyeOffsetX}
+						eyeOffsetY={eyeOffsetY}
+						eyeRadius={eyeRadius}
+					/>
+					<Mouth mouthRadius={mouthRadius} mouthWidth={mouthWidth} />
+				</g>
+			</svg>
+			);
+		</FaceEl>
+	);
+};
+
+const FaceEl = styled.div``;
+export default face;
